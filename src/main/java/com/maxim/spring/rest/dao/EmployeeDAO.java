e393e076456ef451389821403d2f8417740cfdb0
Максим <zaruv14@yandex.ru>
2022-07-02 20:27:51 +0300

    rest API study

---
+++ src/main/java/com/maxim/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.maxim.spring.rest.dao;
+
+import com.maxim.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
